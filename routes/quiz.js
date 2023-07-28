58395035107d3119785472f6f8f8b1b4d2d1eb5f
isa-ismail <fahimismail75.scc@gmail.com>
2023-07-28 17:47:34 +0600

    update

--- routes/quiz.js
+++ routes/quiz.js
@@ -27,6 +27,12 @@ router.get('/:userId', async (req, res, next) => {
 router.post('/', async (req, res, next) => { 
 
     try {
+        const quizz = await QuizSchema.findOne({ userId: req.body.userId });
+        if (quizz) {
+            quiz.score = req.body.score;
+            await quiz.save();
+        }
+        
         const { topic, userId, score } = req.body;
         const quiz = new QuizSchema({ topic, userId, score });
         await quiz.save();
